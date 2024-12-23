a420519a152b80d36f191a3a54b47bafc819f95b
killumination <illuminationth@gmail.com>
2024-12-24 00:46:51 +0900

    no message

---
+++ SltUtil/Packages/SltUtilRoot/Runtime/SltUtilTest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class SltUtilTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public static class FStlUtilTest
+{
+    public static int TestFuncInt() { return 123; }
+    public static float TestFuncFloat() { return 456f; }
+}
